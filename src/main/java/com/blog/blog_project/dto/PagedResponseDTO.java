1cc5a871eba197efdc5333efbf11305dedfb63f4
Orhan Gazi Barak <orhangazibarak@outlook.com>
2025-10-10 16:34:34 +0300

    Added factory method for integrate easily pages reponses to any endpoint.

--- src/main/java/com/blog/blog_project/dto/PagedResponseDTO.java
+++ src/main/java/com/blog/blog_project/dto/PagedResponseDTO.java
@@ -3,6 +3,7 @@ package com.blog.blog_project.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -18,4 +19,17 @@ public class PagedResponseDTO<T> {
     private boolean first;
     private boolean last;
     private boolean empty;
+
+    public static <T> PagedResponseDTO<T> from(Page<?> page, List<T> content) {
+        return new PagedResponseDTO<>(
+                content,
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast(),
+                page.isEmpty()
+        );
+    }
 }
